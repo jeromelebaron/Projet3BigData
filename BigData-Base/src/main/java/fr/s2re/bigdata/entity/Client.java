04d3c4ea0d1c9060b5082327c3d93ee03d4ec1a8
Meriem Ben Rejeb <benrejeb.meriam@gmail.com>
2016-06-17 11:25:37 +0200

    commitClient

---
+++ BigData-Base/src/main/java/fr/s2re/bigdata/entity/Client.java
@@ -0,0 +1,36 @@
+package fr.s2re.bigdata.entity;
+
+import java.util.Random;
+
+public class Client {
+	 
+	private  int id;
+
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Client(int id) {
+		super();
+		this.id = id;
+	}
+
+
+	public Client() {
+		super();
+	}
+
+
+
+
+	  
+
+}
