317eb8ca8094d96a2683fde0f372450dacf5f8fa
Jerdopler <lebaronjerome@free.fr>
2016-06-17 10:47:52 +0200

    Ajout d'un produit

---
+++ BigData-Base/src/main/java/fr/s2re/bigdata/entity/Produit.java
@@ -0,0 +1,14 @@
+/*
+ * Créé le 17 juin 2016 par Jérome LE BARON
+ */
+package fr.s2re.bigdata.entity;
+
+/**
+ * Représente un produit qui sera ajouté dans la base Mongo
+ * @author Jérome LE BARON
+ * @author $LastChangedBy$
+ * @version $Revision$ $Date$
+ */
+public class Produit {
+
+}
